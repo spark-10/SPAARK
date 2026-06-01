d33a7e6eba4d6b59b749f27901c9cdd4bf234d18
spark-10 <kazm.thabt.kazm@gmail.com>
2026-06-01 03:46:13 +0300

    Update route.js

--- src/src/app/api/auth/[...nextauth]/route.js
+++ src/src/app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,26 @@
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
 
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID || "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
+      authorization: { params: { scope: "identify guilds" } },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/',
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
